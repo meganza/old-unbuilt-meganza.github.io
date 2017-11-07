9377f863bb63a7e89f1c86fbd401639c83601bd2
meganza <meganza@gmail.com>
2017-11-07 21:12:55 +0800

    Fixed carousel and added images

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,13 +2,54 @@ console.log('main.js');
 
 $(document).ready(function(){
 
-  $("#showNetsafe").click(function(){
-    if($("#netsafeSlide").is(":visible")){
-      $("#netsafeSlide").hide(1000);
+  $("#showBerkano").click(function(){
+    if($("#berkanoSlide").is(":visible")){
+      $("#berkanoSlide").hide(1000);
     } else{
-      $("#netsafeSlide").show(1000);
+      $("#berkanoSlide").show(1000);
     }
   });
+
+  $("#showTypo").click(function(){
+    if($("#typoSlide").is(":visible")){
+      $("#typoSlide").hide(1000);
+    } else{
+      $("#typoSlide").show(1000);
+    }
+  });
+
+  $("#showDLSU").click(function(){
+    if($("#dlsuSlide").is(":visible")){
+      $("#dlsuSlide").hide(1000);
+    } else{
+      $("#dlsuSlide").show(1000);
+    }
+  });
+
+  $("#showTG").click(function(){
+    if($("#tgSlide").is(":visible")){
+      $("#tgSlide").hide(1000);
+    } else{
+      $("#tgSlide").show(1000);
+    }
+  });
+
+  $("#showInfo").click(function(){
+    if($("#infoSlide").is(":visible")){
+      $("#infoSlide").hide(1000);
+    } else{
+      $("#infoSlide").show(1000);
+    }
+  });
+
+  $("#showIllus").click(function(){
+    if($("#illuSlide").is(":visible")){
+      $("#illuSlide").hide(1000);
+    } else{
+      $("#illuSlide").show(1000);
+    }
+  });
+
 
 });
 
