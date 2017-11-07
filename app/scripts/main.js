cc35d9026c4c5065112c02cf2bddc1661243f9ac
meganza <meganza@gmail.com>
2017-11-08 00:13:11 +0800

    Removed all text fillers

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,54 +2,53 @@ console.log('main.js');
 
 $(document).ready(function(){
 
-  $("#showBerkano").click(function(){
-    if($("#berkanoSlide").is(":visible")){
-      $("#berkanoSlide").hide(1000);
-    } else{
-      $("#berkanoSlide").show(1000);
+  $('#showBerkano').click(function(){
+    if($('#berkanoSlide').is(':visible')){
+      $('#berkanoSlide').slideUp(750);
+    } else {
+      $('#berkanoSlide').slideDown(750);
     }
   });
 
-  $("#showTypo").click(function(){
-    if($("#typoSlide").is(":visible")){
-      $("#typoSlide").hide(1000);
-    } else{
-      $("#typoSlide").show(1000);
+  $('#showTypo').click(function(){
+    if($('#typoSlide').is(':visible')){
+      $('#typoSlide').slideUp(750);
+    } else {
+      $('#typoSlide').slideDown(750);
     }
   });
 
-  $("#showDLSU").click(function(){
-    if($("#dlsuSlide").is(":visible")){
-      $("#dlsuSlide").hide(1000);
-    } else{
-      $("#dlsuSlide").show(1000);
+  $('#showDLSU').click(function(){
+    if($('#dlsuSlide').is(':visible')){
+      $('#dlsuSlide').slideUp(750);
+    } else {
+      $('#dlsuSlide').slideDown(750);
     }
   });
 
-  $("#showTG").click(function(){
-    if($("#tgSlide").is(":visible")){
-      $("#tgSlide").hide(1000);
-    } else{
-      $("#tgSlide").show(1000);
+  $('#showTG').click(function(){
+    if($('#tgSlide').is(':visible')){
+      $('#tgSlide').slideUp(750);
+    } else {
+      $('#tgSlide').slideDown(750);
     }
   });
 
-  $("#showInfo").click(function(){
-    if($("#infoSlide").is(":visible")){
-      $("#infoSlide").hide(1000);
-    } else{
-      $("#infoSlide").show(1000);
+  $('#showInfo').click(function(){
+    if($('#infoSlide').is(':visible')){
+      $('#infoSlide').slideUp(750);
+    } else {
+      $('#infoSlide').slideDown(750);
     }
   });
 
-  $("#showIllus").click(function(){
-    if($("#illuSlide").is(":visible")){
-      $("#illuSlide").hide(1000);
-    } else{
-      $("#illuSlide").show(1000);
+  $('#showIllus').click(function(){
+    if($('#illuSlide').is(':visible')){
+      $('#illuSlide').slideUp(750);
+    } else {
+      $('#illuSlide').slideDown(750);
     }
   });
-
 
 });
 
