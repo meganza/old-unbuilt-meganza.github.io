88b815936c3db6c6122526d674043f39f97554c9
meganza <meganza@gmail.com>
2017-11-07 19:36:46 +0800

    Added carousel for first work and fixed work icons for those with none

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,23 @@
 console.log('main.js');
+
+$(document).ready(function(){
+
+  $("#showNetsafe").click(function(){
+    if($("#netsafeSlide").is(":visible")){
+      $("#netsafeSlide").hide(1000);
+    } else{
+      $("#netsafeSlide").show(1000);
+    }
+  });
+
+});
+
+// function showSlide(num){
+//   console.log(num);
+//   if(num === 1){
+//     var x = document.getElementById("netsafeSlide");
+//     if(x.style.display === "none"){
+//       x.style.display = "block";
+//     }
+//   }
+// }
